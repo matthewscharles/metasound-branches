7c8920d44866cf0d37633c4b8878f788996fd222
Charles Matthews <charles0matthews@gmail.com>
2025-02-22 10:16:46 -0500

    Add Array Route first draft
    This seems to work as expected, so far so good. Repurposing the model from Array Reverse with a simple if statement and assignment to OutArray.
    
    If I want to match the Route nodes in the standard library, should just have on Set, no differentiation between inputs.

---
+++ Source/MetasoundBranches/Private/MetasoundArrayRouteNode.cpp
@@ -0,0 +1,37 @@
+// Copyright 2025 Charles Matthews. All Rights Reserved.
+
+#pragma once
+
+#include "MetasoundBranches/Public/MetasoundArrayRouteNode.h"
+#include "MetasoundTime.h"
+#include "MetasoundNodeRegistrationMacro.h"
+#include "MetasoundDataTypeRegistrationMacro.h"
+
+
+namespace Metasound
+{
+    template class TArrayRouteOperator<TArray<float>>;
+    template class TArrayRouteNode<TArray<float>>;
+    
+    template class TArrayRouteOperator<TArray<int32>>;
+    template class TArrayRouteNode<TArray<int32>>;
+    
+    template class TArrayRouteOperator<TArray<bool>>;
+    template class TArrayRouteNode<TArray<bool>>;
+    
+    template class TArrayRouteOperator<TArray<FTime>>;
+    template class TArrayRouteNode<TArray<FTime>>;
+    
+    
+}
+
+// alias workaround (because I can't use the template class directly in the METASOUND_REGISTER_NODE macro)
+using FRouteFloatNodeAlias  = Metasound::TArrayRouteNode<TArray<float>>;
+using FRouteIntNodeAlias    = Metasound::TArrayRouteNode<TArray<int32>>;
+using FRouteBoolNodeAlias   = Metasound::TArrayRouteNode<TArray<bool>>;
+using FRouteTimeNodeAlias = Metasound::TArrayRouteNode<TArray<Metasound::FTime>>;
+
+METASOUND_REGISTER_NODE(FRouteFloatNodeAlias);
+METASOUND_REGISTER_NODE(FRouteIntNodeAlias);
+METASOUND_REGISTER_NODE(FRouteBoolNodeAlias);
+METASOUND_REGISTER_NODE(FRouteTimeNodeAlias);
