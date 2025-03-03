209ec25951ff497422a5f1fcb0171dfa4002b10c
Charles Matthews <charles0matthews@gmail.com>
2025-03-03 11:24:29 -0500

    Annotations and labels

--- Source/MetasoundBranches/Private/MetasoundBreakNoteFloatNode.cpp
+++ Source/MetasoundBranches/Private/MetasoundBreakNoteFloatNode.cpp
@@ -68,7 +68,7 @@ namespace Metasound
                 Metadata.MajorVersion = 1;
                 Metadata.MinorVersion = 0;
                 Metadata.DisplayName = METASOUND_LOCTEXT("BreakNoteFloatDisplayName", "Break Note (Float)");
-                Metadata.Description = METASOUND_LOCTEXT("BreakNoteFloatDesc", "Provides pitch and velocity from a note array.");
+                Metadata.Description = METASOUND_LOCTEXT("BreakNoteFloatDesc", "Unpacks pitch and velocity from a note array.");
                 Metadata.Author = "Charles Matthews";
                 Metadata.PromptIfMissing = PluginNodeMissingPrompt;
                 Metadata.DefaultInterface = DeclareVertexInterface();
--- Source/MetasoundBranches/Private/MetasoundMakeNoteFloatNode.cpp
+++ Source/MetasoundBranches/Private/MetasoundMakeNoteFloatNode.cpp
@@ -1,5 +1,7 @@
 // Copyright 2025 Charles Matthews. All Rights Reserved.
 
+// Note: this could work with a optional struct to incorporate duration, but it would be less portable; probably better for a pattern event.
+
 #include "MetasoundBranches/Public/MetasoundMakeNoteFloatNode.h"
 #include "MetasoundExecutableOperator.h"
 #include "MetasoundPrimitives.h"
