8278c2272634ce2f7b50fba83e9b5c4f9cc7e4e7
Charles Matthews <charles0matthews@gmail.com>
2025-03-09 09:33:01 -0400

    Create ParseMidi.h
    First draft of MIDI parser

---
+++ Source/MetasoundBranches/Public/ParseMidi.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <cstdint>
+
+namespace Metasound
+{
+    constexpr uint8_t kNoteOffsets[7] = { 0, 2, 4, 5, 7, 9, 11 };
+
+    // direct ascii indexing 
+    constexpr int8_t kAccidentalOffsets[256] = {
+        ['#'] = 1, ['b'] = -1,
+        // avoid garbage values
+        ['0'] = 0, ['1'] = 0, ['2'] = 0, ['3'] = 0, ['4'] = 0, ['5'] = 0, ['6'] = 0, ['7'] = 0, ['8'] = 0, ['9'] = 0,
+        // null terminator
+        ['\0'] = 0, 
+    };
+
+    constexpr int ParseMidi(const char* note)
+    {
+        uint8_t pitchIndex = note[0] - 'a'; 
+
+        int accidental = kAccidentalOffsets[note[1]] + kAccidentalOffsets[note[2]];
+
+        int octave = note[1 + (accidental != 0) + (accidental == 2 || accidental == -2)] - '0';
+
+        return 12 + (octave * 12) + kNoteOffsets[pitchIndex] + accidental;
+    }
+}
