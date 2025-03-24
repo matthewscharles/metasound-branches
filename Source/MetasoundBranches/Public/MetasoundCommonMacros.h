89f3c66ab518fcce4b9eda37d1c990f736a049fb
Charles Matthews <charles0matthews@gmail.com>
2025-03-24 09:21:03 -0400

    Create MetasoundCommonMacros.h
    Need to switch over to bindinputs and bindoutputs, hoping to reduce boilerplate

---
+++ Source/MetasoundBranches/Public/MetasoundCommonMacros.h
@@ -0,0 +1,12 @@
+#define METASOUND_DISABLE_LEGACY_IO()                                     \
+    /* Disable legacy GetInputs/GetOutputs — BindInputs used instead */   \
+    virtual FDataReferenceCollection GetInputs() const override           \
+    {                                                                     \
+        checkNoEntry();                                                   \
+        return {};                                                        \
+    }                                                                     \
+    virtual FDataReferenceCollection GetOutputs() const override          \
+    {                                                                     \
+        checkNoEntry();                                                   \
+        return {};                                                        \
+    }
