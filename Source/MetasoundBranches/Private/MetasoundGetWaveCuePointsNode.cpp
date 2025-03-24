77a5831fa5ccead1086f04d83dea537d83fe9d32
Charles Matthews <charles0matthews@gmail.com>
2025-03-24 14:18:31 -0400

    restore missing category text

--- Source/MetasoundBranches/Private/MetasoundGetWaveCuePointsNode.cpp
+++ Source/MetasoundBranches/Private/MetasoundGetWaveCuePointsNode.cpp
@@ -69,7 +69,10 @@ namespace Metasound
 				Metadata.Author = "Charles Matthews";
 				Metadata.PromptIfMissing = PluginNodeMissingPrompt;
 				Metadata.DefaultInterface = DeclareVertexInterface();
-				Metadata.CategoryHierarchy = { METASOUND_LOCTEXT("Custom", "Branches") };
+				Metadata.CategoryHierarchy = {
+					METASOUND_LOCTEXT("Custom", "Branches"),
+					METASOUND_LOCTEXT("CustomSub", "Wave Asset")
+				};
 				return Metadata;
 			};
 
