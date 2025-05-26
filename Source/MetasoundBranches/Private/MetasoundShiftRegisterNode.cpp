396d2b230ed905944e60709a833a93f115203d0f
Charles Matthews <charles0matthews@gmail.com>
2025-05-26 14:59:56 -0400

    include usual macro

--- Source/MetasoundBranches/Private/MetasoundShiftRegisterNode.cpp
+++ Source/MetasoundBranches/Private/MetasoundShiftRegisterNode.cpp
@@ -7,6 +7,7 @@
 #include "MetasoundFacade.h"
 #include "MetasoundNodeRegistrationMacro.h"
 #include "MetasoundParamHelper.h"
+#include "MetasoundBranches/Public/MetasoundCommonMacros.h"
 
 #define LOCTEXT_NAMESPACE "MetasoundBranches_ShiftRegister"
 
