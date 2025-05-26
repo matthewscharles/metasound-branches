c262ccf04419f2725caca4e7d1f964f8db64693b
Charles Matthews <charles0matthews@gmail.com>
2025-05-26 00:04:21 -0400

    Revert "Update MetasoundSlewConfigurableNode.cpp"
    This reverts commit e4ce3ece0f713a16d79ab757fa26062ebe8ecea5.

--- Source/MetasoundBranches/Private/MetasoundSlewConfigurableNode.cpp
+++ Source/MetasoundBranches/Private/MetasoundSlewConfigurableNode.cpp
@@ -149,6 +149,7 @@ public:
 		for (int32 c = 0; c < Inputs.Num(); ++c)
 		{
 			const int32 Frames = Inputs[c]->Num();
+			Outputs[c]->SetNumUninitialized(Frames);
 
 			const float* In  = Inputs[c]->GetData();
 			float*       Out = Outputs[c]->GetData();
