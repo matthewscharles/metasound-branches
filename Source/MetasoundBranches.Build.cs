53cdd85ce3c1dfa219dee2ad9dac0842b221129c
Charles Matthews <charles0matthews@gmail.com>
2025-05-27 15:27:04 -0400

    include metasoundexperimental as well as runtime

--- Source/MetasoundBranches.Build.cs
+++ Source/MetasoundBranches.Build.cs
@@ -20,6 +20,7 @@ public class MetasoundBranches : ModuleRules
                 "SlateCore",
                 "Projects",
                 // NB: this would disqualify the plugin for FAB, hopefully the CAT material will make it into the 5.6 release
+                "MetasoundExperimental",
                 "MetasoundExperimentalRuntime"
             }
         );
