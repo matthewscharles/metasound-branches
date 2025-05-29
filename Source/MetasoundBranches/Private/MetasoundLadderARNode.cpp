e0fda58fb219ee687c21f6368ce284259ac9c557
Charles Matthews <charles0matthews@gmail.com>
2025-05-29 11:16:28 -0400

    looks like I forgot to include the cat header

--- Source/MetasoundBranches/Private/MetasoundLadderARNode.cpp
+++ Source/MetasoundBranches/Private/MetasoundLadderARNode.cpp
@@ -1,6 +1,7 @@
 // Copyright 2025 Charles Matthews. All Rights Reserved.
 
 #include "MetasoundBranches/Public/MetasoundLadderARNode.h"
+#include "MetasoundChannelAgnosticType.h"
 #include "MetasoundExecutableOperator.h"
 #include "MetasoundPrimitives.h"
 #include "MetasoundNodeRegistrationMacro.h"
