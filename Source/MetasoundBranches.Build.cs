beca940cad0dfd7dc7188002a4d377c0a9e07147
Charles Matthews <charles0matthews@gmail.com>
2025-12-14 22:16:06 -0500

    add audioexperimentalruntime to dependencies

--- Source/MetasoundBranches.Build.cs
+++ Source/MetasoundBranches.Build.cs
@@ -21,6 +21,7 @@ public class MetasoundBranches : ModuleRules
                 "Projects",
                 "AudioExtensions",
                 "SignalProcessing",
+                "AudioExperimentalRuntime",
                 "MetasoundExperimentalRuntime",
                 "CoreUObject",
                 "Engine",
